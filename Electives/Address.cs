45c9811e8cee4131d711b03fb35da6eaeadadc04
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-09-25 16:28:00 +0300

    Вернул адрес

---
+++ Electives/Address.cs
@@ -0,0 +1,59 @@
+﻿namespace Electives
+{
+	internal class Address
+	{
+		string region_;
+		string city_;
+		string street_;
+		string house_;
+		string building_;
+
+		/// <summary>
+		/// Адрес 
+		/// </summary>
+		/// <param name="region">Административный субъект</param>
+		/// <param name="city">Город</param>
+		/// <param name="street">Улица</param>
+		/// <param name="house">Дом</param>
+		/// <param name="building">Номер строения</param>
+		Address(string region, string city, string street,
+			string house, string building = "")
+		{
+			this.region_ = region;
+			this.city_ = city;
+			this.street_ = street;
+			this.house_ = house;
+			this.building_ = building;
+		}
+
+		public string region
+		{
+			get => region_;
+			set => region_ = value;
+		}
+
+		public string city
+		{
+			get => city_;
+			set => city_ = value;
+		}
+
+		public string street
+		{
+			get => street_;
+			set => street_ = value;
+		}
+
+		public string house
+		{
+			get => house_;
+			set => house_ = value;
+		}
+
+		public string building
+		{
+			get => building_;
+			set => building_ = value;
+		}
+	}
+}
