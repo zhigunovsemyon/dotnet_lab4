299d21c6cc79fd42841c694e174e172be9907a37
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-07 22:03:38 +0300

    Создал enum для оценок

---
+++ Electives/Mark.cs
@@ -0,0 +1,21 @@
+﻿namespace Electives
+{	
+	/// <summary>Оценка</summary>
+	public enum Mark : sbyte
+	{
+		/// <summary>Пустая ячейка</summary>
+		None,
+		/// <summary>Пропуск</summary>
+		Skip,
+		/// <summary>Два</summary>
+		Two,
+		/// <summary>Три</summary>
+		Three,
+		/// <summary>Четыре</summary>
+		Four,
+		/// <summary>Пять</summary>
+		Five,
+		/// <summary>Особое отличие</summary>
+		Extra
+	}
+}
