d4a58e0a8b8a5386f19eec916be620c9e15df726
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-09 09:46:54 +0300

    Сделал оценку классом, дал ToString

--- Electives/Mark.cs
+++ Electives/Mark.cs
@@ -1,21 +1,42 @@
 ﻿namespace Electives
-{	
+{
 	/// <summary>Оценка</summary>
-	public enum Mark : sbyte
+	public class Mark
 	{
-		/// <summary>Пустая ячейка</summary>
-		None,
-		/// <summary>Пропуск</summary>
-		Skip,
-		/// <summary>Два</summary>
-		Two,
-		/// <summary>Три</summary>
-		Three,
-		/// <summary>Четыре</summary>
-		Four,
-		/// <summary>Пять</summary>
-		Five,
-		/// <summary>Особое отличие</summary>
-		Extra
-	}
+		public enum Type : byte
+		{
+			/// <summary>Пустая ячейка</summary>
+			None,
+			/// <summary>Пропуск</summary>
+			Skip,
+			/// <summary>Два</summary>
+			Two,
+			/// <summary>Три</summary>
+			Three,
+			/// <summary>Четыре</summary>
+			Four,
+			/// <summary>Пять</summary>
+			Five,
+			/// <summary>Особое отличие</summary>
+			Extra
+		}
+
+		public Type value { get; set; } = Type.None;
+
+		public override string ToString()
+		{
+			return value switch
+			{
+				Type.None => " ",
+				Type.Skip => "н",
+				Type.Two => "2",
+				Type.Three => "3",
+				Type.Four => "4",
+				Type.Five => "5",
+				Type.Extra => "5*",
+				_ => throw new NotImplementedException()
+			};
+		}
+
+	};
 }
