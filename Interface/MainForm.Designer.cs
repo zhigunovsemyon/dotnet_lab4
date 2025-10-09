5d25a903c230aca086228963b5c8ed66137b7632
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-09 11:39:58 +0300

    Добавил все кнопушки

--- Interface/MainForm.Designer.cs
+++ Interface/MainForm.Designer.cs
@@ -28,33 +28,77 @@
 		private void InitializeComponent ()
 		{
 			menuStrip1 = new MenuStrip();
-			FileMenuItem = new ToolStripMenuItem();
+			FileToolStripMenuItem = new ToolStripMenuItem();
 			CloseButton = new ToolStripMenuItem();
+			StudentsToolStripMenuItem = new ToolStripMenuItem();
+			ClassesToolStripMenuItem = new ToolStripMenuItem();
+			StudentAddtoolStripMenuItem = new ToolStripMenuItem();
+			StudentEditToolStripMenuItem = new ToolStripMenuItem();
+			ClassAddStripMenuItem = new ToolStripMenuItem();
+			ClassEditStripMenuItem = new ToolStripMenuItem();
 			menuStrip1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// menuStrip1
 			// 
-			menuStrip1.Items.AddRange(new ToolStripItem[] { FileMenuItem });
+			menuStrip1.Items.AddRange(new ToolStripItem[] { FileToolStripMenuItem, StudentsToolStripMenuItem, ClassesToolStripMenuItem });
 			menuStrip1.Location = new Point(0, 0);
 			menuStrip1.Name = "menuStrip1";
 			menuStrip1.Size = new Size(800, 24);
 			menuStrip1.TabIndex = 0;
 			menuStrip1.Text = "menuStrip1";
 			// 
-			// FileMenuItem
+			// FileToolStripMenuItem
 			// 
-			FileMenuItem.DropDownItems.AddRange(new ToolStripItem[] { CloseButton });
-			FileMenuItem.Name = "FileMenuItem";
-			FileMenuItem.Size = new Size(48, 20);
-			FileMenuItem.Text = "&Файл";
+			FileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { CloseButton });
+			FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+			FileToolStripMenuItem.Size = new Size(48, 20);
+			FileToolStripMenuItem.Text = "&Файл";
 			// 
 			// CloseButton
 			// 
 			CloseButton.Name = "CloseButton";
-			CloseButton.Size = new Size(180, 22);
+			CloseButton.Size = new Size(109, 22);
 			CloseButton.Text = "&Выйти";
 			CloseButton.Click += CloseButton_Click;
+			// 
+			// StudentsToolStripMenuItem
+			// 
+			StudentsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { StudentAddtoolStripMenuItem, StudentEditToolStripMenuItem });
+			StudentsToolStripMenuItem.Name = "StudentsToolStripMenuItem";
+			StudentsToolStripMenuItem.Size = new Size(71, 20);
+			StudentsToolStripMenuItem.Text = "&Студенты";
+			// 
+			// ClassesToolStripMenuItem
+			// 
+			ClassesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ClassAddStripMenuItem, ClassEditStripMenuItem });
+			ClassesToolStripMenuItem.Name = "ClassesToolStripMenuItem";
+			ClassesToolStripMenuItem.Size = new Size(63, 20);
+			ClassesToolStripMenuItem.Text = "&Занятия";
+			// 
+			// StudentAddtoolStripMenuItem
+			// 
+			StudentAddtoolStripMenuItem.Name = "StudentAddtoolStripMenuItem";
+			StudentAddtoolStripMenuItem.Size = new Size(180, 22);
+			StudentAddtoolStripMenuItem.Text = "&Добавить";
+			// 
+			// StudentEditToolStripMenuItem
+			// 
+			StudentEditToolStripMenuItem.Name = "StudentEditToolStripMenuItem";
+			StudentEditToolStripMenuItem.Size = new Size(180, 22);
+			StudentEditToolStripMenuItem.Text = "&Редактировать";
+			// 
+			// ClassAddStripMenuItem
+			// 
+			ClassAddStripMenuItem.Name = "ClassAddStripMenuItem";
+			ClassAddStripMenuItem.Size = new Size(180, 22);
+			ClassAddStripMenuItem.Text = "&Добавить";
+			// 
+			// ClassEditStripMenuItem
+			// 
+			ClassEditStripMenuItem.Name = "ClassEditStripMenuItem";
+			ClassEditStripMenuItem.Size = new Size(180, 22);
+			ClassEditStripMenuItem.Text = "&Редактировать";
 			// 
 			// MainForm
 			// 
@@ -74,7 +118,13 @@
 		#endregion
 
 		private MenuStrip menuStrip1;
-		private ToolStripMenuItem FileMenuItem;
+		private ToolStripMenuItem FileToolStripMenuItem;
 		private ToolStripMenuItem CloseButton;
+		private ToolStripMenuItem StudentsToolStripMenuItem;
+		private ToolStripMenuItem ClassesToolStripMenuItem;
+		private ToolStripMenuItem StudentAddtoolStripMenuItem;
+		private ToolStripMenuItem StudentEditToolStripMenuItem;
+		private ToolStripMenuItem ClassAddStripMenuItem;
+		private ToolStripMenuItem ClassEditStripMenuItem;
 	}
 }
