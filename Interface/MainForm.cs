94d2be73bd2e9d2d27f4188bd401952038f0dad4
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-10 22:07:52 +0300

    XML-комментарии в основной форме

--- Interface/MainForm.cs
+++ Interface/MainForm.cs
@@ -2,9 +2,13 @@ using Electives;
 
 namespace Interface
 {
+	/// <summary> Основная форма </summary>
 	public partial class MainForm : Form
 	{
+		/// <summary> Внутреннее поле со студентом </summary>
 		private Electives.Student student_ = new();
+
+		/// <summary> Внутреннее поле с занятием </summary>
 		private Electives.Class class_ = new();
 
 		public MainForm ()
@@ -12,11 +16,16 @@ namespace Interface
 			InitializeComponent();
 		}
 
+		/// <summary> Метод для закрытия приложения через пункт меню </summary>
 		private void CloseButton_Click (object sender, EventArgs e)
 		{
 			Close();
 		}
 
+		/// <summary>
+		/// Метод для вызова и обработки результата работы формы изменения студента
+		/// </summary>
+		/// <param name="student">Обрабатываемый студент</param>
 		private void AddOrEditStudent (Electives.Student student)
 		{
 			var form = new StudentEditForm(student);
@@ -39,18 +48,24 @@ namespace Interface
 			this.student_ = form.student;
 		}
 
+		/// <summary> Обработчик поля создания нового студента </summary>
 		private void StudentAddtoolStripMenuItem_Click (object sender, EventArgs e)
 		{
 			//Вызов формы на чистом студенте
 			AddOrEditStudent(new Student());
 		}
 
+		/// <summary> Обработчик редактирования существующего студента </summary>
 		private void StudentEditToolStripMenuItem_Click (object sender, EventArgs e)
 		{
 			//Вызов формы на копии исходного студента
 			AddOrEditStudent(student_.clone());
 		}
 
+		/// <summary>
+		/// Метод для вызова и обработки результата работы формы изменения занятия
+		/// </summary>
+		/// <param name="class">Обрабатываемое занятие</param>
 		private void AddOrEditClass(Electives.Class @class)
 		{			
 			var form = new ClassEditForm(@class);
@@ -73,11 +88,13 @@ namespace Interface
 			this.@class_ = form.@class;
 		}
 
+		/// <summary> Обработчик создания нового занятия </summary>
 		private void ClassAddStripMenuItem_Click (object sender, EventArgs e)
 		{
 			AddOrEditClass(new Class());
 		}
 
+		/// <summary> Обработчик редактирования существующего занятия </summary>
 		private void ClassEditStripMenuItem_Click (object sender, EventArgs e)
 		{
 			AddOrEditClass(class_.clone());
