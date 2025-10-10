b1e04c817adeeea299ad60b1f533ba8cf414563a
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-10 13:41:32 +0300

    Отредактировал параметры окна

--- Interface/ClassEditForm.Designer.cs
+++ Interface/ClassEditForm.Designer.cs
@@ -27,10 +27,19 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "ClassEditForm";
+			SuspendLayout();
+			// 
+			// ClassEditForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			Name = "ClassEditForm";
+			ShowIcon = false;
+			Text = "Информация о предмете";
+			ResumeLayout(false);
 		}
 
 		#endregion
