dc1da8249b4a98b0068d9874af925a6c9b087f7e
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-10 13:45:18 +0300

    Добавил свойство с полем занятий формы

--- Interface/ClassEditForm.cs
+++ Interface/ClassEditForm.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Interface
+﻿namespace Interface
 {
 	public partial class ClassEditForm : Form
 	{
-		public ClassEditForm ()
+		public ClassEditForm (Electives.Class @class)
 		{
 			InitializeComponent();
+			this.@class = @class;
 		}
 	}
+
+	public Electives.Class @class { get; set; }
 }
