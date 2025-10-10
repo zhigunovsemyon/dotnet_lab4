9b34f6f4e7f71e94bc3e829f6ef594990237b798
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-10 18:53:31 +0300

    Добавил кнопки на панель занятий, реализовал закрытие

--- Interface/ClassEditForm.cs
+++ Interface/ClassEditForm.cs
@@ -2,11 +2,17 @@
 {
 	public partial class ClassEditForm : Form
 	{
-		public ClassEditForm (Electives.Class @class)
+		public ClassEditForm(Electives.Class @class)
 		{
 			InitializeComponent();
 			this.@class = @class;
 		}
 		public Electives.Class @class { get; set; }
+
+		private void OkButton_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+		}
 	}
 }
