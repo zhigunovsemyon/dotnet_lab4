a28617f98056c5dbd0617e88e50f2eac6624be58
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-10 10:15:43 +0300

    Отрефакторил вызов формы по DRY

--- Interface/MainForm.cs
+++ Interface/MainForm.cs
@@ -7,38 +7,48 @@ namespace Interface
 		private Electives.Student student_ = new();
 		//private Electives.Class class_;
 
-		public MainForm()
+		public MainForm ()
 		{
 			InitializeComponent();
 		}
 
-		private void CloseButton_Click(object sender, EventArgs e)
+		private void CloseButton_Click (object sender, EventArgs e)
 		{
 			Close();
 		}
 
-		private void StudentAddtoolStripMenuItem_Click(object sender, EventArgs e)
+		private void AddOrEditStudent(Electives.Student student)
 		{
-			var form = new StudentEditForm(new Student());
-			if (DialogResult.OK == form.ShowDialog()) {
-				if (form.student != null) {
-					if (form.student.IsValid) {
-						student_ = form.student;
-					}
-				}
+			var form = new StudentEditForm(student);
+			if (DialogResult.OK != form.ShowDialog()) {
+				return;
 			}
+
+			if (form.student == null) {
+				MessageBox.Show(
+					"StudentEditForm вернула null",
+					"Внутренняя ошибка"
+				);
+				return;
+			}
+			if (!form.student.IsValid) {
+				MessageBox.Show("Неправильно указаны данные!");
+				return;
+			}
+
+			this.student_ = form.student;
 		}
 
-		private void StudentEditToolStripMenuItem_Click(object sender, EventArgs e)
+		private void StudentAddtoolStripMenuItem_Click (object sender, EventArgs e)
+		{	
+			//Вызов формы на чистом студенте
+			AddOrEditStudent(new Student());
+		}
+
+		private void StudentEditToolStripMenuItem_Click (object sender, EventArgs e)
 		{
-			var form = new StudentEditForm(student_.clone());
-			if (DialogResult.OK == form.ShowDialog()) {
-				if (form.student != null) {
-					if (form.student.IsValid) {
-						student_ = form.student;
-					}
-				}
-			}
+			//Вызов формы на копии исходного студента
+			AddOrEditStudent(student_.clone());
 		}
 	}
 }
